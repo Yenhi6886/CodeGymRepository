536b50eadbea2f4562e71d7583c3945212813f67
Yenhi6886 <nguyenyenhi220904@gmail.com>
2025-08-21 09:44:07 +0700

    add

---
+++ Module5/Hook/hook-custom/src/App.jsx
@@ -0,0 +1,12 @@
+import MyClock from "./component/MyClock";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="Container">
+      <MyClock />
+    </div>
+  );
+}
+
+export default App;
